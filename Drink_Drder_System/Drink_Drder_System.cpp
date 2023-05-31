2db5ebbf15bb11c5ffdcdf0292a0130740e6cdae
HungYuanChu <56665108+HungYuanChu@users.noreply.github.com>
2023-05-31 23:14:35 +0800

    drink order system

--- Drink_Drder_System/Drink_Drder_System.cpp
+++ Drink_Drder_System/Drink_Drder_System.cpp
@@ -1,20 +1,47 @@
-﻿// Drink_Drder_System.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "DrinkItem.h"
+#include "Drink_Drder_System.h"
 
-#include <iostream>
+void AddNewDrink(std::vector<DrinkItem>& drinks)
+{
+    drinks.push_back(DrinkItem("紅茶", "中杯", 30));
+    drinks.push_back(DrinkItem("紅茶", "大杯", 40));
+    drinks.push_back(DrinkItem("綠茶", "中杯", 35));
+    drinks.push_back(DrinkItem("綠茶", "大杯", 45));
+    drinks.push_back(DrinkItem("奶茶", "中杯", 50));
+    drinks.push_back(DrinkItem("奶茶", "大杯", 60));
+    drinks.push_back(DrinkItem("烏龍茶", "中杯", 25));
+    drinks.push_back(DrinkItem("烏龍茶", "大杯", 30));
+}
+void DisplayDrinkMenu(vector<DrinkItem>& drinks) {
+    cout << setw(4) << "編號" << setw(10) << "飲料名稱" << setw(10) << "大小" << setw(10) << "售價" << endl;
+    cout << "--------------------------------------" << endl;
 
+    /*int i = 1;
+    for (DrinkItem item : drinks) {
+        cout << setw(4) << i;
+        item.displayItem();
+        i++;
+    }*/
+
+    int i = 1;
+    vector<DrinkItem>::iterator v = drinks.begin();
+    while (v != drinks.end()) {
+        cout << setw(4) << i;
+        v->displayItem();
+        v++;
+        i++;
+    }
+    {
+
+    }
+    cout << "--------------------------------------" << endl;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<DrinkItem> drinks;
+    AddNewDrink(drinks);
+    DisplayDrinkMenu(drinks);
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
